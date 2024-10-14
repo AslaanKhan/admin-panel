d9967a0173686ada9459676735242aecdb5f07b4
Arslaan khan <arslaan.tech@gmail.com>
2024-10-14 21:53:17 +0530

    latest changes

--- src/app/dashboard/offers/page-Arsalaan.tsx
+++ src/app/dashboard/offers/page-Arsalaan.tsx
@@ -1,8 +1,9 @@
-"use client";
+"use client"
 import { DataTable } from "@/components/global/dataTable";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { getAllOffers, toggleOffer } from "@/services/offer.service";
 import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { ColumnDef } from "@tanstack/react-table";
@@ -11,19 +12,22 @@ import React, { useState, useEffect } from "react";
 import Swal from "sweetalert2";
 
 export type Offer = {
-  _id: string;
-  productIds: string[];
-  discountPercentage?: number;
-  conditions?: {
-      minQuantity?: number;
-      discountPerVal?: number;
-  };
-  startDate: Date;
-  endDate: Date;
-  code?: string;
-  isActive: boolean;
+    _id: string;
+    productIds: [{
+        product: string,
+        productName: string,
+    }]; // Keep this for backend
+    discountPercentage?: number;
+    flatDiscount?: number;
+    conditions?: {
+        minQuantity?: number;
+        discountPerVal?: number;
+    };
+    startDate: Date;
+    endDate: Date;
+    code?: string;
+    isActive: boolean;
 };
-
 
 export default function Offers() {
     const [data, setOffers] = useState<Offer[]>([]);
@@ -34,7 +38,7 @@ export default function Offers() {
             id: "select",
             header: ({ table }) => (
                 <Checkbox
-                    checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
+                    checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && true)}
                     onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
                     aria-label="Select all"
                 />
@@ -50,34 +54,49 @@ export default function Offers() {
             enableHiding: false,
         },
         {
+            accessorKey: "code",
+            header: "Name / Code",
+            cell: ({ row }) => <div className="text-center">{row?.original?.code}</div>, // Display product names
+        },
+        {
+            accessorKey: "productNames",
+            header: "Products",
+            cell: ({ row }) => <div className="text-center">{row?.original?.productIds.map((product, i) => (`${product.productName}${row?.original?.productIds.length - 1 > i ? ' ,' : ''}`))}</div>, // Display product names
+        },
+        {
             accessorKey: "discountPercentage",
             header: "Discount (%)",
-            cell: ({ row }) => <div>{row.getValue("discountPercentage") || "N/A"}</div>,
+            cell: ({ row }) => <div className="text-center">{row.original.discountPercentage || "N/A"}</div>,
+        },
+        {
+            accessorKey: "flatDiscount",
+            header: "Flat Discount (%)",
+            cell: ({ row }) => <div className="text-center">{row.original.flatDiscount || "N/A"}</div>,
         },
         {
             accessorKey: "conditions.minQuantity",
             header: "Min Quantity",
-            cell: ({ row }) => <div>{row.original.conditions?.minQuantity || "N/A"}</div>,
+            cell: ({ row }) => <div className="text-center">{row.original.conditions?.minQuantity || "N/A"}</div>,
         },
         {
             accessorKey: "conditions.discountPerVal",
             header: "Discount per KG",
-            cell: ({ row }) => <div>{row.original.conditions?.discountPerVal || "N/A"}</div>,
+            cell: ({ row }) => <div className="text-center">{row.original.conditions?.discountPerVal || "N/A"}</div>,
         },
         {
             accessorKey: "startDate",
             header: "Start Date",
-            cell: ({ row }) => <div>{new Date(row.getValue("startDate")).toLocaleDateString()}</div>,
+            cell: ({ row }) => <div className="text-center">{new Date(row.getValue("startDate")).toLocaleDateString()}</div>,
         },
         {
             accessorKey: "endDate",
             header: "End Date",
-            cell: ({ row }) => <div>{new Date(row.getValue("endDate")).toLocaleDateString()}</div>,
+            cell: ({ row }) => <div className="text-center">{new Date(row.getValue("endDate")).toLocaleDateString()}</div>,
         },
         {
             accessorKey: "isActive",
             header: "Active",
-            cell: ({ row }) => <div>{row.getValue("isActive") ? "Yes" : "No"}</div>,
+            cell: ({ row }) => <div className="text-center">{row.getValue("isActive") ? "Yes" : "No"}</div>,
         },
         {
             id: "actions",
@@ -95,14 +114,23 @@ export default function Offers() {
                         <DropdownMenuItem onClick={() => navigator.clipboard.writeText(row.original._id)}>
                             Copy Offer ID
                         </DropdownMenuItem>
+                        <DropdownMenuItem
+                            onClick={async () =>
+                                await toggleOffer(row.original._id, {
+                                    isActive: !row.original?.isActive,
+                                }).then(() => fetchData())
+                            }
+                        >
+                             {row.original.isActive ? `Deactivate` : "Activate"} Offer
+                        </DropdownMenuItem>
                         <DropdownMenuSeparator />
                         <DropdownMenuItem onClick={() => router.push(`/dashboard/offers/${row.original._id}`)}>
                             View or Edit Offer
                         </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem onClick={() => deleteOffer(row.original)}>
+                        {/* <DropdownMenuSeparator /> */}
+                        {/* <DropdownMenuItem onClick={() => deleteOffer(row.original)}>
                             Delete Offer
-                        </DropdownMenuItem>
+                        </DropdownMenuItem> */}
                     </DropdownMenuContent>
                 </DropdownMenu>
             ),
@@ -111,9 +139,8 @@ export default function Offers() {
 
     const fetchData = async () => {
         try {
-            // Uncomment and implement the API call to fetch offers
-            // const response = await getAllOffers();
-            // setOffers(response?.offers);
+            const response = await getAllOffers();
+            setOffers(response?.offers); // Set the new offers state
         } catch (error) {
             console.error("Error fetching offers:", error);
         }
@@ -146,5 +173,9 @@ export default function Offers() {
         }
     };
 
-    return <DataTable<Offer> columns={columns} data={data} filterField="discountPercentage" />;
+    const createNewOffer = () => {
+        router.push("/dashboard/offers/new");
+    }
+
+    return <DataTable<Offer> columns={columns} data={data} onCreateRecord={createNewOffer} filterField="discountPercentage" />;
 }
